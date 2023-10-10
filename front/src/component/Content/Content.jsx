a83127ca2d11ab8096e87cec7588bfeca218f3d5
01KingMaker <fabrakoto0987@gmail.com>
2023-10-10 02:50:35 +0200

    last update

--- front/src/component/Content/Content.jsx
+++ front/src/component/Content/Content.jsx
@@ -1,1 +1,19 @@
-class Content
+import React from 'react'
+import './Content.css'
+
+class Content extends React.Component{
+    render(){
+        return(
+            <div className="styleContent">
+                <div className="styleImageGroup">
+
+                </div>
+                <div className="styleFilter">
+
+                </div>
+            </div>
+            )
+    }
+}
+
+export default Content
